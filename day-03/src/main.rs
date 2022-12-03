252ad320cedde3d89279fce2bc3d597cc3230e24
Jeff Walden <jwalden@mit.edu>
2022-12-03 01:59:46 -0800

    Finish day 3.

---
+++ day-03/src/main.rs
@@ -0,0 +1,57 @@
+use itertools::Itertools;
+
+static CONTENTS: &str = include_str!("../input");
+
+fn to_priority(c: char) -> u64 {
+    if ('a'..='z').contains(&c) {
+        (c as u64 - 'a' as u64) + 1
+    } else if ('A'..='Z').contains(&c) {
+        (c as u64 - 'A' as u64) + 27
+    } else {
+        panic!("bad char: {}", c);
+    }
+}
+
+fn priorities(s: &str) -> u64 {
+    s.chars().fold(0u64, |acc, c| acc | (1 << to_priority(c)))
+}
+
+fn part1() {
+    let sum = CONTENTS
+        .lines()
+        .map(|line| {
+            let (first, second) = line.split_at(line.len() / 2);
+
+            let first_priorities = priorities(first);
+            let second_priorities = priorities(second);
+
+            let shared = first_priorities & second_priorities;
+            shared.trailing_zeros() as u64
+        })
+        .fold(0u64, |acc, p| acc + p);
+
+    print!("sum of priorities: {}\n", sum);
+}
+
+fn part2() {
+    let sum = CONTENTS
+        .lines()
+        .into_iter()
+        .chunks(3)
+        .into_iter()
+        .map(|mut sacks| {
+            let first = priorities(sacks.next().unwrap());
+            let second = priorities(sacks.next().unwrap());
+            let third = priorities(sacks.next().unwrap());
+
+            (first & second & third).trailing_zeros() as u64
+        })
+        .fold(0u64, |acc, p| acc + p);
+
+    print!("sum of badges for rucksack triplets: {}\n", sum);
+}
+
+fn main() {
+    part1();
+    part2();
+}
