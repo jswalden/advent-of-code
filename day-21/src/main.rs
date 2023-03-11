80012a2e9f5e3169b88886257be01e8d8c142f0d
Jeff Walden <jwalden@mit.edu>
2023-03-11 02:23:15 -0800

    Finish day 21 part 2.

--- day-21/src/main.rs
+++ day-21/src/main.rs
@@ -1,12 +1,15 @@
 use std::collections::{HashMap, HashSet};
 
 type Monkey = String;
+type MonkeyNumber = i64;
 
+#[derive(PartialEq, Eq)]
 enum YellType {
     Add,
     Sub,
     Div,
     Mul,
+    Equ,
 }
 struct YellEquation {
     yell_type: YellType,
@@ -14,6 +17,14 @@ struct YellEquation {
 }
 
 impl YellEquation {
+    fn is_equality(&self) -> bool {
+        if let YellType::Equ = self.yell_type {
+            true
+        } else {
+            false
+        }
+    }
+
     fn evaluate(&self, resolved: &HashMap<Monkey, MonkeyNumber>) -> Option<MonkeyNumber> {
         let x = *(resolved.get(&self.args.0)?);
         let y = *(resolved.get(&self.args.1)?);
@@ -22,7 +33,44 @@ impl YellEquation {
             YellType::Sub => x - y,
             YellType::Div => x / y,
             YellType::Mul => x * y,
+            YellType::Equ => return None,
         })
+    }
+
+    fn solve_for(
+        &self,
+        resolved: &HashMap<Monkey, MonkeyNumber>,
+        value: MonkeyNumber,
+    ) -> (MonkeyNumber, Monkey) {
+        match (resolved.get(&self.args.0), resolved.get(&self.args.1)) {
+            (Some(x), None) => {
+                let unknown_monkey = self.args.1.clone();
+
+                let value = match self.yell_type {
+                    YellType::Add => value - *x,
+                    YellType::Sub => *x - value,
+                    YellType::Div => *x / value,
+                    YellType::Mul => value / *x,
+                    YellType::Equ => panic!("shouldn't see root equation"),
+                };
+
+                (value, unknown_monkey)
+            }
+            (None, Some(y)) => {
+                let unknown_monkey = self.args.0.clone();
+
+                let value = match self.yell_type {
+                    YellType::Add => value - *y,
+                    YellType::Sub => value + *y,
+                    YellType::Div => value * *y,
+                    YellType::Mul => value / *y,
+                    YellType::Equ => panic!("shouldn't see root equation"),
+                };
+
+                (value, unknown_monkey)
+            }
+            _ => panic!("should only have one side undefined"),
+        }
     }
 }
 
@@ -31,7 +79,14 @@ enum Yell {
     Equation(YellEquation),
 }
 
-type MonkeyNumber = i64;
+impl Yell {
+    fn is_equality(&self) -> bool {
+        match self {
+            &Yell::Number(_) => false,
+            &Yell::Equation(ref eqn) => eqn.is_equality(),
+        }
+    }
+}
 
 struct MonkeyYell {
     name: Monkey,
@@ -66,101 +121,169 @@ fn parse_monkey_yell(s: &str) -> MonkeyYell {
     }
 }
 
-struct MonkeyGraph {
-    resolved: HashMap<Monkey, MonkeyNumber>,
-}
-
+#[derive(PartialEq, Eq)]
 enum Part {
     Part1,
     Part2,
 }
 
-impl MonkeyGraph {
-    fn new(input: &str, part: Part) -> MonkeyGraph {
-        struct PartialGraph {
-            resolved: HashMap<Monkey, MonkeyNumber>,
-            unresolved: HashMap<Monkey, YellEquation>,
-            deps: HashMap<Monkey, HashSet<Monkey>>,
-            part: Part,
-        }
-        impl PartialGraph {
-            fn new(part: Part) -> PartialGraph {
-                PartialGraph {
-                    resolved: HashMap::new(),
-                    unresolved: HashMap::new(),
-                    deps: HashMap::new(),
-                    part,
-                }
-            }
+const YOU_NAME: &str = "humn";
 
-            fn process_yelled_number(&mut self, monkey_name: Monkey, number: MonkeyNumber) {
-                self.resolved.insert(monkey_name.clone(), number);
-
-                if let Some(waiting_monkeys) = self.deps.remove(&monkey_name) {
-                    for waiting_monkey_name in waiting_monkeys {
-                        let eqn = self
-                            .unresolved
-                            .get(&waiting_monkey_name)
-                            .expect("unresolved");
-
-                        if let Some(n) = eqn.evaluate(&self.resolved) {
-                            self.process_yelled_number(waiting_monkey_name, n);
-                        }
-                    }
-                }
-            }
-
-            fn add_dependency(&mut self, from: Monkey, to: &Monkey) {
-                if self.resolved.contains_key(to) {
-                    return;
-                }
-
-                self.deps
-                    .entry(to.clone())
-                    .or_insert(HashSet::new())
-                    .insert(from);
-            }
-
-            fn process_yelled_equation(&mut self, monkey_name: Monkey, eqn: YellEquation) {
-                self.add_dependency(monkey_name.clone(), &eqn.args.0);
-                self.add_dependency(monkey_name.clone(), &eqn.args.1);
-
-                if let Some(n) = eqn.evaluate(&self.resolved) {
-                    self.process_yelled_number(monkey_name, n);
-                } else {
-                    self.unresolved.insert(monkey_name, eqn);
-                }
-            }
-
-            fn process_monkey_yell(&mut self, monkey_name: Monkey, yell: Yell) {
-                match yell {
-                    Yell::Number(number) => self.process_yelled_number(monkey_name, number),
-                    Yell::Equation(eqn) => self.process_yelled_equation(monkey_name, eqn),
-                }
-            }
-        }
-
-        let mut graph = PartialGraph::new(part);
-
-        for MonkeyYell { name, yell } in input.trim().lines().map(parse_monkey_yell) {
-            graph.process_monkey_yell(name, yell);
-        }
-
-        assert!(graph.deps.is_empty());
-        // graph.unresolved still has entries in it.
-
-        MonkeyGraph {
-            resolved: graph.resolved,
+struct MonkeyInfo {
+    resolved: HashMap<Monkey, MonkeyNumber>,
+    unresolved: HashMap<Monkey, YellEquation>,
+    deps: HashMap<Monkey, HashSet<Monkey>>,
+    part: Part,
+}
+impl MonkeyInfo {
+    fn new(part: Part) -> MonkeyInfo {
+        MonkeyInfo {
+            resolved: HashMap::new(),
+            unresolved: HashMap::new(),
+            deps: HashMap::new(),
+            part,
         }
     }
 
-    fn monkey_number(&self, monkey: Monkey) -> MonkeyNumber {
-        *self.resolved.get(&monkey).expect("monkey name")
+    fn process_yelled_number(&mut self, monkey_name: Monkey, number: MonkeyNumber) {
+        self.resolved.insert(monkey_name.clone(), number);
+
+        if let Some(waiting_monkeys) = self.deps.remove(&monkey_name) {
+            for waiting_monkey_name in waiting_monkeys {
+                let eqn = self
+                    .unresolved
+                    .get(&waiting_monkey_name)
+                    .expect("unresolved");
+
+                if let Some(n) = eqn.evaluate(&self.resolved) {
+                    self.process_yelled_number(waiting_monkey_name, n);
+                }
+            }
+        }
+    }
+
+    fn add_dependency(&mut self, from: Monkey, to: &Monkey) {
+        if self.resolved.contains_key(to) {
+            return;
+        }
+
+        self.deps
+            .entry(to.clone())
+            .or_insert(HashSet::new())
+            .insert(from);
+    }
+
+    fn process_yelled_equation(&mut self, monkey_name: Monkey, eqn: YellEquation) {
+        self.add_dependency(monkey_name.clone(), &eqn.args.0);
+        self.add_dependency(monkey_name.clone(), &eqn.args.1);
+
+        assert!(
+            eqn.yell_type != YellType::Equ || (self.part == Part::Part2 && monkey_name == "root"),
+            "shouldn't see == until part 2, on root"
+        );
+
+        if let Some(n) = eqn.evaluate(&self.resolved) {
+            self.process_yelled_number(monkey_name, n);
+        } else {
+            self.unresolved.insert(monkey_name, eqn);
+        }
+    }
+
+    fn process_monkey_yell(&mut self, monkey_name: Monkey, yell: Yell) {
+        match yell {
+            Yell::Number(number) => self.process_yelled_number(monkey_name, number),
+            Yell::Equation(eqn) => self.process_yelled_equation(monkey_name, eqn),
+        }
+    }
+
+    fn find_missing_number(
+        &self,
+        mut known_number: MonkeyNumber,
+        mut unknown_monkey: Monkey,
+    ) -> MonkeyNumber {
+        loop {
+            if self.resolved.contains_key(&unknown_monkey) {
+                return known_number;
+            }
+
+            if unknown_monkey == YOU_NAME {
+                return known_number;
+            }
+
+            let unknown_eqn = self
+                .unresolved
+                .get(&unknown_monkey)
+                .expect("unknown_monkey");
+
+            (known_number, unknown_monkey) = unknown_eqn.solve_for(&self.resolved, known_number);
+        }
+    }
+
+    fn compute_required_number(&self) -> MonkeyNumber {
+        assert!(
+            self.part == Part::Part2,
+            "required number is only in part 2"
+        );
+
+        let root_eq = self
+            .unresolved
+            .get(&String::from("root"))
+            .expect("root eqn");
+
+        let (ref x, ref y) = root_eq.args;
+
+        let (known_number, unknown_monkey) = if let Some(n) = self.resolved.get(x) {
+            (*n, y.clone())
+        } else {
+            (*self.resolved.get(y).expect("resolved y"), x.clone())
+        };
+
+        self.find_missing_number(known_number, unknown_monkey)
     }
 }
 
-fn root_monkey_yells(input: &str, part: Part) -> MonkeyNumber {
-    MonkeyGraph::new(input, part).monkey_number("root".to_string())
+fn compute_part1_root_monkey_number(input: &str) -> MonkeyNumber {
+    let mut info = MonkeyInfo::new(Part::Part1);
+
+    for MonkeyYell { name, yell } in input.trim().lines().map(parse_monkey_yell) {
+        info.process_monkey_yell(name, yell);
+    }
+
+    assert!(info.deps.is_empty());
+    // graph.unresolved still has entries in it.
+
+    *info
+        .resolved
+        .get(&String::from("root"))
+        .expect("root monkey")
+}
+
+fn compute_part2_required_monkey_number(input: &str) -> MonkeyNumber {
+    let mut info = MonkeyInfo::new(Part::Part2);
+
+    for MonkeyYell { name, yell } in input
+        .trim()
+        .lines()
+        .map(parse_monkey_yell)
+        .filter(|&MonkeyYell { ref name, ref yell }| name != YOU_NAME || yell.is_equality())
+        .map(|MonkeyYell { name, yell }| MonkeyYell {
+            name: name.clone(),
+            yell: if name != "root" {
+                yell
+            } else if let Yell::Equation(eqn) = yell {
+                Yell::Equation(YellEquation {
+                    yell_type: YellType::Equ,
+                    args: eqn.args,
+                })
+            } else {
+                panic!("unexpected root number");
+            },
+        })
+    {
+        info.process_monkey_yell(name, yell);
+    }
+
+    info.compute_required_number()
 }
 
 #[test]
@@ -181,18 +304,25 @@ lgvd: ljgn * ptdq
 drzm: hmdt - zczc
 hmdt: 32";
 
-    let root_yells = root_monkey_yells(INPUT);
+    let root_yells = compute_part1_root_monkey_number(INPUT);
     println!("Part 1 root yells: {root_yells}");
-    assert_eq!(root_yells, 152)
+    assert_eq!(root_yells, 152);
+
+    let required_yells = compute_part2_required_monkey_number(INPUT);
+    println!("Part 2 required yells: {required_yells}");
+    assert_eq!(required_yells, 301);
 }
 
 fn main() {
     static INPUT: &str = include_str!("../input");
 
     // Part 1.
-    let root_yells = root_monkey_yells(INPUT);
+    let root_yells = compute_part1_root_monkey_number(INPUT);
     println!("Part 1 root yells: {root_yells}");
     assert_eq!(root_yells, 75_147_370_123_646);
 
     // Part 2.
+    let required_yells = compute_part2_required_monkey_number(INPUT);
+    println!("Part 2 required yells: {required_yells}");
+    assert_eq!(required_yells, 3_423_279_932_937);
 }
